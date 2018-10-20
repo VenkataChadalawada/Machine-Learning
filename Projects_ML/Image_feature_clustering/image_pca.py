c585a103d4fd05221c00c0e06e11ea7a20ff18df
Venkata Chadalawada <venchad@gmail.com>
2018-10-19 19:54:22 -0500

    Add files via upload

---
+++ Projects_ML/Image_feature_clustering/image_pca.py
@@ -0,0 +1,25 @@
+import pandas as pd
+samples = pd.read_csv('lcd-digits.csv', header=None)
+digit = samples.iloc[0,:].values # .values => converting into list
+print(digit)
+
+def show_as_image(sample):
+    bitmap = sample.reshape((13, 8))
+    plt.figure()
+    plt.imshow(bitmap, cmap='gray', interpolation='nearest')
+    plt.colorbar()
+    plt.show()
+    
+# Import PCA
+from sklearn.decomposition import PCA
+
+# Create a PCA instance: model
+model = PCA(n_components=7)
+
+# Apply fit_transform to samples: features
+features = model.fit_transform(samples)
+
+# Call show_as_image on each component
+for component in model.components_:
+    show_as_image(component) 
+    
